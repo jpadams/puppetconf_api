2361994efb5868862858589aba9bd7a4c9e2d07a
jpadams <jeremy@puppetlabs.com>
2015-10-09 14:54:58 -0700

    fix demo to vm

--- 08_ds2.sh
+++ 08_ds2.sh
@@ -3,4 +3,4 @@ curl -X PUT -H 'Content-Type: application/json' \
 --cert   `puppet config print hostcert` \
 --key    `puppet config print hostprivkey` \
 --cacert `puppet config print localcacert` \
-https://learning.puppetlabs.demo:4433/rbac-api/v1/ds | python -m json.tool
+https://learning.puppetlabs.vm:4433/rbac-api/v1/ds | python -m json.tool
