0611c466b4ab5d5deaaf59737b3ace8485f2f543
Jeremy Adams <jeremy@puppetlabs.com>
2015-10-11 13:19:14 -0700

    windows examples

---
+++ x_windows_status1.ps1
@@ -0,0 +1,2 @@
+# Plain HTTP
+Invoke-WebRequest -Uri http://learning.puppetlabs.vm:8123/status/v1/services
---
+++ x_windows_status2.ps1
@@ -0,0 +1,14 @@
+# Install chocolatey on your Windows agent via the Master
+# and install curl package with chocolatey
+#
+# package { 'curl': provider => chocolatey, }
+
+# Similar to Linux invocation. Note '&' to invoke command, 'curl.exe'
+# so Invoke-WebRequest is not called with its alias 'curl', and '`'
+# backtick characters used as line continuation. I left CR-LF in.
+ 
+& curl.exe -X GET `
+--cert "$(puppet config print hostcert)" `
+--key "$(puppet config print hostprivkey)" `
+--cacert "$(puppet config print localcacert)" `
+https://master.inf.puppetlabs.demo:4433/status/v1/services
