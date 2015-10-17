8de6467e00d02b78ca41486d129039f08f039ef3
jpadams <jeremy@puppetlabs.com>
2015-10-17 09:01:49 -0700

    example of both backticks and dollarsign parens

--- 07_ds1.sh
+++ 07_ds1.sh
@@ -6,3 +6,13 @@ curl -X GET \
 --key    $(puppet config print hostprivkey) \
 --cacert $(puppet config print localcacert) \
 https://learning.puppetlabs.vm:4433/rbac-api/v1/ds | python -m json.tool
+
+# This backticks syntax also works on *nix.
+# Run the `puppet config print` commands on the
+# command line to see the output.
+#curl -X GET \
+#--tlsv1 \
+#--cert   `puppet config print hostcert` \
+#--key    `puppet config print hostprivkey` \
+#--cacert `puppet config print localcacert` \
+#https://learning.puppetlabs.vm:4433/rbac-api/v1/ds | python -m json.tool
